5d10a776e028b49bc18daf646070b5908d9aa45e
meital.kozhidov <meital.kozhidov@infinitylabs.co.il>
2024-02-21 18:33:08 +0200

    minor style adjust

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -5,7 +5,10 @@ import '@/assets/styles/App.css';
 const App = () => {
   return (
     <div className="grid-design">
-      <div className="left-half"> Welcome to Dashboard</div>
+      <div className="left-half">
+        <div className="greeting">Welcome to Dashboard</div>
+        <div> Here will be a description of the website.</div>
+      </div>
       <div className="right-half"> USER LOGIN</div>
     </div>
   );
