7a6ee3874f952bcf075d628b8d389831826c9762
meital.kozhidov <meital.kozhidov@infinitylabs.co.il>
2024-03-17 00:01:39 +0200

    removed redundant eslint rule

--- fe/src/main.tsx
+++ fe/src/main.tsx
@@ -9,7 +9,6 @@ import '@/assets/styles/index.css';
 
 /******************************************************************************/
 
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
