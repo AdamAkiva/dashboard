3888fceb2461199db36bcc9699b5b9cf95c7a3a2
Adam Akiva <adam.akiva@infinitylabs.co.il>
2024-03-19 18:17:26 +0200

    Added event emitter async handler for the tests as well

--- be/__tests__/config/vite.config.ts
+++ be/__tests__/config/vite.config.ts
@@ -11,6 +11,7 @@ const defaultConfig: UserConfig = {
   teardownTimeout: 4_000,
   bail: 1,
   globalSetup: './__tests__/config/globalSetup.ts',
+  setupFiles: './__tests__/config/setup.ts',
   restoreMocks: true,
   logHeapUsage: true,
   fileParallelism: true,
